bcf2c7be1690f93d96a308e5fbfe9ff529bffcb2
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-03-10 00:49:43 +0100

    🎨 Separate installation entity from application

--- github/api/app_client.py
+++ github/api/app_client.py
@@ -13,15 +13,17 @@ from gidgethub.sansio import Event
 import jwt
 
 # pylint: disable=relative-beyond-top-level
+from ...app.runtime.context import RUNTIME_CONTEXT
+# pylint: disable=relative-beyond-top-level
 from ...utils.asynctools import (
     amap, dict_to_kwargs_cb,
 )
 # pylint: disable=relative-beyond-top-level
 from ..config.app import GitHubAppIntegrationConfig
 # pylint: disable=relative-beyond-top-level
-from ..models import (
-    GitHubAppInstallation, GitHubInstallationAccessToken,
-)
+from ..entities.app_installation import GitHubAppInstallation
+# pylint: disable=relative-beyond-top-level
+from ..models import GitHubAppInstallation as GitHubAppInstallationModel
 from .client import GitHubAPIClient
 from .tokens import GitHubJWTToken
 
@@ -79,8 +81,8 @@ class GitHubApp(AbstractAsyncContextManager):
             logger.info(
                 '* Installation id %s (expires at %s, installed to %s)',
                 install_id,
-                install_val['access'].expires_at,
-                install_val['data'].account['login'],
+                install_val._token.expires_at,
+                install_val._metadata.account['login'],
             )
 
         return self
@@ -116,50 +118,40 @@ class GitHubApp(AbstractAsyncContextManager):
         """The GitHub App client with an async CM interface."""
         return GitHubAPIClient(self.gh_jwt)
 
-    async def get_install_token(self, *, access_token_url):
-        """Retrieve installation access token from GitHub API."""
-        async with self.github_client as gh_api:
-            return GitHubInstallationAccessToken(**(await gh_api.post(
-                access_token_url,
-                data=b'',
-                accept='application/vnd.github.machine-man-preview+json',
-            )))
-
     async def add_installation(self, event):
         """Retrieve an installation creds from store."""
         install = event.data['installation']
         install_id = install['id']
-        self._installations[install_id] = {
-            'data': GitHubAppInstallation(install),
-            'access': await self.get_install_token(
-                access_token_url=install['access_tokens_url'],
-            ),
-        }
+        self._installations[install_id] = GitHubAppInstallation(install)
+        await self._installations[install_id].retrieve_access_token()
         return self._installations[install_id]
 
     async def get_installation(self, event):
         """Retrieve an installation creds from store."""
         if event.event == 'ping':
-            return None
+            return GitHubAppInstallation(None)
 
         install_id = event.data['installation']['id']
-        return self._installations.get(install_id)
+        app_installation = self._installations.get(install_id)
+
+        # pylint: disable=assigning-non-slot
+        RUNTIME_CONTEXT.app_installation = (
+            app_installation
+        )
+
+        return app_installation
 
     async def get_installations(self):
         """Retrieve all installations with access tokens via API."""
         installations = defaultdict(dict)
         async with self.github_client as gh_api:
             async for install in amap(
-                    dict_to_kwargs_cb(GitHubAppInstallation),
+                    dict_to_kwargs_cb(GitHubAppInstallationModel),
                     gh_api.getiter(
                         '/app/installations',
                         preview_api_version='machine-man',
                     ),
             ):
-                installations[install.id] = {
-                    'data': install,
-                    'access': await self.get_install_token(
-                        access_token_url=install.access_tokens_url,
-                    ),
-                }
+                installations[install.id] = GitHubAppInstallation(install)
+                await installations[install.id].retrieve_access_token()
         return installations
