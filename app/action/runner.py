ca0e50ce536d1f3e030b8c57ccc11dc67d12985f
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-03-11 14:25:07 +0100

    ✨ Add a GitHub Action entrypoint API

---
+++ app/action/runner.py
@@ -0,0 +1,46 @@
+"""Octomachinery CLI runner for GitHub Action environments."""
+
+import asyncio
+import logging
+
+# pylint: disable=relative-beyond-top-level
+from ...github.entities.action import GitHubAction
+# pylint: disable=relative-beyond-top-level
+from ..config import BotAppConfig
+# pylint: disable=relative-beyond-top-level
+from ..routing.webhooks_dispatcher import (
+    route_github_action_event,
+)
+# pylint: disable=relative-beyond-top-level
+from ..runtime.context import RUNTIME_CONTEXT
+
+
+logger = logging.getLogger(__name__)
+
+
+async def process_github_action(config):
+    """Schedule GitHub Action event for processing."""
+    logger.info('Processing GitHub Action event...')
+
+    github_action = GitHubAction(config.action)
+    logger.info('GitHub Action=%r', config.action)
+
+    await route_github_action_event(github_action)
+
+
+def run():
+    """Start up a server using CLI args for host and port."""
+    config = BotAppConfig.from_dotenv()
+    RUNTIME_CONTEXT.config = config  # pylint: disable=assigning-non-slot
+    logging.basicConfig(
+        level=logging.DEBUG
+        if config.runtime.debug  # pylint: disable=no-member
+        else logging.INFO,
+    )
+    try:
+        asyncio.run(process_github_action(config))
+    except KeyboardInterrupt:
+        logger.info(
+            ' Terminating the GitHub Action event processing flow '.
+            center(50, '='),
+        )
