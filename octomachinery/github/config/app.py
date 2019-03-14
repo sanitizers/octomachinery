704ee74eef56dad3f5d5103ae70e4accbcb516ac
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-03-15 00:00:40 +0100

    🎨 Don't require an app id and a key for Actions

--- octomachinery/github/config/app.py
+++ octomachinery/github/config/app.py
@@ -5,17 +5,32 @@ import environ
 # pylint: disable=relative-beyond-top-level
 from ...app.runtime.context import RUNTIME_CONTEXT
 # pylint: disable=relative-beyond-top-level
+from ...app.runtime.utils import detect_env_mode
+# pylint: disable=relative-beyond-top-level
 from ..models.utils import SecretStr
+
+
+def validate_is_not_none_if_app(self, attr, value):
+    if value is None and detect_env_mode() == 'app':
+        raise ValueError(
+            f'GitHub App must provide a proper value for {attr!r}',
+        )
 
 
 @environ.config  # pylint: disable=too-few-public-methods
 class GitHubAppIntegrationConfig:
     """GitHub App auth related config."""
 
-    app_id = environ.var(name='GITHUB_APP_IDENTIFIER')
+    app_id = environ.var(
+        None,
+        name='GITHUB_APP_IDENTIFIER',
+        validator=validate_is_not_none_if_app,
+    )
     private_key = environ.var(
+        None,
         name='GITHUB_PRIVATE_KEY',
         converter=SecretStr,
+        validator=validate_is_not_none_if_app,
     )
     webhook_secret = environ.var(
         None, name='GITHUB_WEBHOOK_SECRET',
