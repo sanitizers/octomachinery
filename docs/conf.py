9b82193a7b9c49ed402e8dbd85f7ba7ce38ab767
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-03-16 23:59:12 +0100

    📝 Integrate sphinxcontrib.apidoc

--- docs/conf.py
+++ docs/conf.py
@@ -83,6 +83,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.apidoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
