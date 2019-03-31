107e96f6d8f213b62d296453eec407b9312fad18
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-03-31 01:52:40 +0100

    🚑 Allow capturing all actions in handlers

--- octomachinery/app/routing/__init__.py
+++ octomachinery/app/routing/__init__.py
@@ -16,12 +16,22 @@ dispatch_event = WEBHOOK_EVENTS_ROUTER.dispatch  # pylint: disable=invalid-name
 process_event = WEBHOOK_EVENTS_ROUTER.register  # pylint: disable=invalid-name
 
 
-def process_event_actions(event_name, actions):
+def process_event_actions(event_name, actions=None):
     """Subscribe to multiple events."""
+    if actions is None:
+        actions = []
+
     def decorator(original_function):
+
         def wrapper(*args, **kwargs):
             return original_function(*args, **kwargs)
+
+        if not actions:
+            wrapper = process_event(event_name)(wrapper)
+
         for action in actions:
             wrapper = process_event(event_name, action=action)(wrapper)
+
         return wraps(original_function)(wrapper)
+
     return decorator
