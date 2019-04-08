7214d3eaab3aa27088d0dbde4e107a1b94e7e632
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-04-08 02:00:15 +0200

    📝🎨 Wire up gidgethub docs via intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -328,6 +328,7 @@ epub_exclude_files = ['search.html']
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'https://docs.python.org/': None,
+    'https://gidgethub.readthedocs.io/en/latest/': None,
 }
 
 # -- Options for todo extension ----------------------------------------------
