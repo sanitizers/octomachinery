619c87b6539878b1956fa4df8318c0917c50910e
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-08-05 00:10:21 +0200

    Add tests for Action outcomes

--- tests/app/action/runner_test.py
+++ tests/app/action/runner_test.py
@@ -8,13 +8,32 @@ import pytest
 from octomachinery.app.action.config import GitHubActionConfig
 from octomachinery.app.action.runner import run
 from octomachinery.app.config import BotAppConfig
+from octomachinery.app.routing import process_event
 from octomachinery.app.runtime.config import RuntimeConfig
 from octomachinery.app.server.config import WebServerConfig
 from octomachinery.github.config.app import GitHubAppIntegrationConfig
+from octomachinery.github.models.action_outcomes import ActionNeutral
+
+
+@process_event('unmatched_event', action='happened')
+async def unmatched_event_happened(action):  # pylint: disable=unused-argument
+    """Handle an unmached event."""
+
+
+@process_event('check_run', action='created')
+async def check_run_created(action):  # pylint: disable=unused-argument
+    """Handle a check_run event."""
+    assert False
+
+
+@process_event('neutral_event', action='qwerty')
+async def neutral_event_qwerty(action):  # pylint: disable=unused-argument
+    """Handle a neutral_event."""
+    ActionNeutral('Neutral outcome').raise_it()
 
 
 @pytest.fixture
-def event_file(tmp_path_factory):
+def event_file(tmp_path_factory, request):
     """Generate a sample JSON event file."""
     gh_event = (
         tmp_path_factory.mktemp('github-action-event') /
@@ -23,6 +42,7 @@ def event_file(tmp_path_factory):
     with gh_event.open('w') as gh_event_file:
         json.dump(
             {
+                'action': request.param,  # 'created',
             },
             gh_event_file,
         )
@@ -30,7 +50,7 @@ def event_file(tmp_path_factory):
 
 
 @pytest.fixture
-def config(monkeypatch, event_file):
+def config(monkeypatch, event_file, request):
     """Create a dummy GitHub Action config."""
     monkeypatch.setattr(
         'octomachinery.app.runtime.utils.detect_env_mode',
@@ -43,7 +63,7 @@ def config(monkeypatch, event_file):
             action='Test Action',
             actor='username-or-bot',
             repository='org/repo',
-            event_name='check_run',
+            event_name=request.param,  # 'check_run'
             event_path=str(event_file),  # '/github/workflow/event.json'
             workspace='/github/workspace',
             sha='e6d4abcb8a6cd989d41ee',
@@ -55,7 +75,21 @@ def config(monkeypatch, event_file):
     )
 
 
-def test_run(config):
+@pytest.mark.parametrize(
+    'config, event_file, expected_return_code',
+    (
+        ('check_run', 'created', 1),
+        ('unmatched_event', 'closed', 0),
+        ('neutral_event', 'qwerty', 78),
+    ),
+    indirect=('config', 'event_file'),
+)
+def test_action_processing_return_code(config, expected_return_code):
     """Test an empty action processing run."""
-    with pytest.raises(SystemExit, match=r'^0$'):
+    with pytest.raises(
+            SystemExit,
+            match=r'^'
+            f'{expected_return_code}'
+            r'$',
+    ):
         run(config=config)
