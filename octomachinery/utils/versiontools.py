9b88068fb1c2551d54d3aa30c742fb6fa622a6a6
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-08-17 18:08:18 +0200

    🐛 Fix shadowing `version` var @ `versiontools`

--- octomachinery/utils/versiontools.py
+++ octomachinery/utils/versiontools.py
@@ -3,7 +3,8 @@
 import os
 from typing import Callable, Optional, Union
 
-from setuptools_scm import get_version, version
+from setuptools_scm import get_version
+from setuptools_scm.version import ScmVersion
 
 
 def get_version_from_scm_tag(
@@ -11,7 +12,7 @@ def get_version_from_scm_tag(
         root: str = '.',
         relative_to: Optional[str] = None,
         local_scheme: Union[
-            Callable[[version.ScmVersion], str], str,
+            Callable[[ScmVersion], str], str,
         ] = 'node-and-date',
 ) -> str:
     """Retrieve the version from SCM tag in Git or Hg."""
