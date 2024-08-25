116222c2e0d788b2a0ea370c2a829a59b15843d1
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2024-08-25 15:29:13 +0200

    🚑 Follow-up fixup for fd183dd
    Ref #61

--- octomachinery/github/api/app_client.py
+++ octomachinery/github/api/app_client.py
@@ -116,12 +116,10 @@ class GitHubApp:
         """Retrieve an installation with access tokens via API."""
         return GitHubAppInstallation(
             dict_to_kwargs_cb(GitHubAppInstallationModel)(
-                **(
-                    await self.api_client.getitem(
-                        '/app/installations/{installation_id}',
-                        url_vars={'installation_id': install_id},
-                        preview_api_version='machine-man',
-                    )
+                await self.api_client.getitem(
+                    '/app/installations/{installation_id}',
+                    url_vars={'installation_id': install_id},
+                    preview_api_version='machine-man',
                 ),
             ),
             self,
